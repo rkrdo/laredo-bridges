ae7ac84d1252fd86ff98daa9148687746f7f8150
Ricardo Cruz <ricardocruz.a89@gmail.com>
2015-04-03 22:08:22 -0500

    Registering the helpers

--- app/app.js
+++ app/app.js
@@ -2,6 +2,8 @@ import Ember from 'ember';
 import Resolver from 'ember/resolver';
 import loadInitializers from 'ember/load-initializers';
 import config from './config/environment';
+import availableHelper from './helpers/available';
+import availableLanesHelper from './helpers/available-lanes';
 
 Ember.MODEL_FACTORY_INJECTIONS = true;
 
@@ -12,5 +14,8 @@ var App = Ember.Application.extend({
 });
 
 loadInitializers(App, config.modulePrefix);
+
+Ember.Handlebars.registerBoundHelper('available', availableHelper); // TODO convert to HTMLBars
+Ember.Handlebars.registerBoundHelper('available-lanes', availableLanesHelper); // TODO convert to HTMLBars
 
 export default App;
